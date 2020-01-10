bccca3e3affc8477f54503434ae0d5dba5e5a2e2
maaryhabad <maryh.abad@gmail.com>
2020-01-10 04:24:09 -0300

    ficha pronta

---
+++ js/dinamico.js
@@ -0,0 +1,132 @@
+var pecas = [ //JSON (JavaScript Object Notation)
+    {
+        "peca18": {
+            "id": "18",
+            "imagem": "images/18/prous2015p61.jpg",
+            "legenda": "Figura Humana (H. Sapiens)",
+            "descricao": "\"Tomei a peça de pedra, e ao observá-la e interessar-me por ela, com Herrero 'y' [e?] Espinosa, Lozada a ofereceu-me com total desinteresse, oferecimento que aceitei de imediato, e pedindo-lhe notícia de como a havia encontrado, contestou: 'Por causalidade, Senhor Chefe. Foram meus filhos que a encontraram, como disse, num dia de chuva, no meio do mesmo caminho por onde você já passou muitas vezes com seu carro. Certamente ele passou por onde estava a cabeça, quando começava a aparecer. Meus filhos, que são uns travessos, com uma colher começaram a escavar ao seu redor, e como a não conseguiam tirar, pois parecia como que cravada, foram em casa, selaram 'un petizo', e com um laço a enlaçaram pelo pescoço e começaram a tirar, até que a arrancaram da terra, e, a arrastando, a levaram para casa'.\"",
+            "citDescricao": "(Meneghin, 2006, p. 3)",
+            "citacoes": ["Serrano, 1933, pl. VI; 1940, p. 418;", "Sierra y Sierra, 1931, p. 91-98;", "Devincenzi, 1928, pl. 12;", "Sosa, 1957;", "Hugarte, 1969, p. 37;", "Prous, 1974, p. 19;", "Meneghin, 2006;", "Domí­nguez, 2015"],
+            "estado": "Uruguai/Rio Negro<br><i>(Prous, 1974, p.19)</i>",
+            "sitio": "Perto de Mercedes<br><i>(Op. cit.)</i>",
+            "contexto": "Ver descrição.<br><br>Encontrado em 1892.",
+            "citContexto": "(Delgado Domínguez, 2015)",
+            "paradeiro": "ex-Coleção Saturnino A. Camps;<br>ex-Coleção Julio Herrera y Obres/Montevideo.<br>ex-Museo Nacional de Historia Natural/Montevideo.<br>ex-Museo de Arte Prehistorica y Indígena/Montevideo;<br>Atual Centro Histórico y Geográfico de Soriano/Mercedes.",
+            "citParadeiro": "(Sierra y Sierra, 1931, p. 98;)<br>(Prous, 1974, p. 19;)<br> Delgado Domínguez, 2015, p. 109.)",
+            "materiaPrima": "\"El mineral que se ha construído el ídolo es, seguramente, granito: granito compacto; por lo mismo, se habrá tallado con relativa facilidad. Sin embargo, el pulimento no es esmerado ni se ha bruñido, por más que todo el objeto ha sido alisado. Podría ser el granito criollo de las inagotables canteras de la Colonia y Conchillas, con incrustaciones de cuarzo blanco, que recuerda la variedad llamada pecmatita, granito abortado cuyos cristales hialinos simulan caracteres hebraicos o arabescos. Así aparecen en el ídolo numerosos lunares... blancos...!\"<br><br>\"El estudio petrográfico con ayuda de una lupa (x12) y luz natural ha demostrado que se trata de una roca ígnea. Esta roca esta compuesta por una pasta máfica de textura dolerítica con cristales submilimétricos equigranulares y con escasos fenocristales de feldspato subcentimétricos. No se ha observado orientación definida de los cristales.\"<br>\"Máfico\", corresponde a rochas ricas em magnésio e ferro, que geralmente têm coloração escura e grãos médios.",
+            "citMateria": ["(Sierra y Sierra, 1931, p. 97-98.)", "(Meneghin, 2006, p. 6)"],
+            "dimensoes": "\"La cabeza es grande: mide 15 centímetros, correspondiendo 4 a la frente y 11 a la cara, cuyo ancho máximo es de 10 centímetros.-Las arcadas supraorbitarias, bien señaladas, reunidas, tienen 6 cm.; la nariz, con un caballete recto de 4 cm., tiene 2 cm. de ancho, y tiene indicada (Sic) las narinas por dos pequeñas depresiones poco escavadas. La boca, de labios finos y entreabiertos, mide transversalmente 3cm.8: la distancia naso-bucal (altura del labio superior), alcanza a 12 milímetros. Las orejas, bien simétricas, ocupan una extensión de 5 cm. verticalmente por 3,5 transversalmente; hacen un relieve moderado, pudiéndose notar el contorno de la hélice unida en sus extremos a la antihélice: un surco poco profundo separa a estos dos pliegues. Adelante, en la concavidad de la antihélice y separado completamente de ésta por otro surco, se nota un relieve casi circular representando el trago. La región que corresponde al cuello, así como la parte superior del tórax es plana en el sentido vertical, débilmente convexa en el transversal. En los ángulos superiores del pecho se nota la implantación de los brazos: el muñón del hombro, en escaso relieve, tiene 18 milímetros de ancho; el brazo, plegado hacia atrás, tiene sus bordes poco marcados, perdiéndose finalmente a los 8 cm. del hombro por su continuidad con el borde de una de las depresiones posteriores que luego señalaremos. Observando la cara posterior del antropolito se nota que solo la región cefálica es débilmente convexa en todo sentido: el resto esta ocupado por dos depresiones poco excavadas, de forma elipsoidal, y con bordes difusos, separados por un puente de solo unos milímetros de ancho. -La depresión superior, más pequeña, empieza a cuatro centímetros del extremo superior; tiene 10,5 cm. de eje vertical y sus bordes externos corresponden, en su parte inferior, a los bordes de los brazos, plegados hacia atrás, según hemos dicho anteriormente. La depresión inferior tiene 19,5 cm. de eje vertical: su extremidad inferior llega hasta 5 cm. del extremo inferior de la pieza, y sus bordes externos, muy débilmente marcados, se continúan con los bordes de los brazos sin transición de ninguna especie, desvaneciéndose progresivamente hacia abajo. Los bordes del antropolito presentan una ranura de escasa profundidad que se extiende desde una axila hasta la otra, pasando por el extremo inferior. Limitada por bordes poco salientes, esta ranura presenta su ancho máximo en su origen axilar, donde alcanza a 15 mm., disminuyendo progresivamente hasta presentar solo 3 mm. en el medio de la extremidad inferior.\"",
+            "citDimensoes": "(Op. cit)",
+            "cavidade": "\"En el pecho se encuentra la depresión característica de todas las esculturas antropo o zoomorfas americanos. La cavidad mide 8cm.5 en el sentido vertical, 7cm.5 en el horizontal y tiene una profundidad máxima de 1cm.5. Perfectamente pulida, está limitada por un borde saliente de 1 cm. de grueso, formando un rectángulo muy regular. Una distancia de 6 cm. separa su borde superior del reborde del mentón; 21 cm. separan su borde inferior del extremo de la pieza, la cual en esta región esta difusa y débilmente excavada\"",
+            "citCavidade": "(Op. cit)",
+            "analise": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut est vitae ante condimentum ultricies ut eget risus. Suspendisse lectus justo, tristique porta turpis at, rutrum volutpat metus. Nullam nec molestie massa. Nullam facilisis suscipit porta. Sed a lobortis dolor, eget finibus augue. Cras volutpat in felis quis egestas. Morbi laoreet laoreet nulla, vitae sagittis nibh tincidunt eget. Aenean placerat lectus sit amet augue dapibus sollicitudin. Aenean tristique porta turpis, nec egestas justo congue vitae.",
+            "observacoes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut est vitae ante condimentum ultricies ut eget risus. Suspendisse lectus justo, tristique porta turpis at, rutrum volutpat metus. Nullam nec molestie massa. Nullam facilisis suscipit porta. Sed a lobortis dolor, eget finibus augue. Cras volutpat in felis quis egestas. Morbi laoreet laoreet nulla, vitae sagittis nibh tincidunt eget. Aenean placerat lectus sit amet augue dapibus sollicitudin. Aenean tristique porta turpis, nec egestas justo congue vitae.",
+            "galeria": ["images/18/sierraysierra1931n1.jpg", "images/18/sosa1957p101.jpg","images/18/prous1974.jpg", "images/18/meneghin2006p21-22.jpg", "images/18/meneghin2006p23.jpg", "images/18/meneghin2006p24.jpg", "images/18/prous2015.jpg", "images/18/localização.jpg", "images/18/localização-2.jpg", "images/18/prous2015p61PLANOS.jpg", "images/18/patrimonioutuguayMAPIexpoMAM2015.jpg", "images/18/mundodelmuseocom.jpg", "images/18/dominguez2015p113.jpg", "images/18/dominguez2015p115.jpg"],
+            "citGaleria": ["Sosa, 1957, p. 101.", "Sosa, 1957.", "Prous, 1974, p. 18.", "Meneghin, 2006, p. 21-22.", "Op. cit., p. 23.","Op. cit., p. 24.", "Prous, 2015, p. 61.", "Meneghin, 2006, p. 19.", "Op. cit.", "Com planos anatômicos. Por Filipi Pompeu.", "Retirado de: <a href=\"https://www.patrimoniouruguay.gub.uy/innovaportal/v/75871/35/mecweb/exposicion?parentid=64016\">Patrimonio Uruguai</a>", "Retirado de: <a href=\"http://www.mundodelmuseo.com/ficha.php?id=1131\">Mundo del Museo</a>", "Delgado Domínguez, 2015, p. 113.", "Op. cit. p. 115."]
+        }
+    }
+]
+
+window.onload = function() {
+    this.document.title = "Peça 18";
+    var numeroDaPeca = pecas[0].peca18;
+    var nomeDaPeca = numeroDaPeca.id + ": " + numeroDaPeca.legenda
+    console.log(nomeDaPeca)
+
+    //Nome da peça
+    $("#nomeDaPeca").html(nomeDaPeca)
+
+    //Imagem
+    $("#imagemPrincipal").src = numeroDaPeca.imagem;
+
+    //Legenda
+    $("#legenda").html(numeroDaPeca.legenda);
+
+    //Descrição
+    $("#descricao").html(numeroDaPeca.descricao);
+
+    //CitaçãoDescricao
+    $("#citacaoDescricao").html(numeroDaPeca.citDescricao);
+
+    //Estado
+    $("#estado").html(numeroDaPeca.estado);
+
+    //Sítio
+    $("#sitio").html(numeroDaPeca.sitio);
+
+    //Contexto
+    $("#contexto").html(numeroDaPeca.contexto);
+
+    //CitaçãoContexto
+    $("#citacaoContexto").html(numeroDaPeca.citContexto);
+
+    //Matéria Prima
+    $("#materiaPrima").html(numeroDaPeca.materiaPrima);
+
+    //Citação da Matéria Prima
+    $("#citMateria").html(numeroDaPeca.citMateria);
+
+    //Dimensões
+    $("#dimensoes").html(numeroDaPeca.dimensoes);
+
+    //Citação das Dimensões
+    $("#citDimensoes").html(numeroDaPeca.citDimensoes);
+
+    //Observações
+    $("#observacoes").html(numeroDaPeca.observacoes);
+
+    //Paradeiro
+    $("#paradeiro").html(numeroDaPeca.paradeiro);
+
+    //Citações de Paradeiro
+    $("#citParadeiro").html(numeroDaPeca.citParadeiro);
+
+    //Cavidade
+    $("#cavidade").html(numeroDaPeca.cavidade);
+
+    //Citação Cavidade
+    $("#citacaoCavidade").html(numeroDaPeca.citCavidade);
+
+    //Análise Ontográfica
+    $("#analise").html(numeroDaPeca.analise);
+
+    var imageGaleria = numeroDaPeca.galeria
+    var citacaoGaleria = numeroDaPeca.citGaleria
+    console.log(imageGaleria)
+    console.log(citacaoGaleria)
+
+    for (i=0; i<imageGaleria.length; i++) {
+        //Cria os elementos
+        var divColunas = document.createElement("div");
+        var a = document.createElement("a");
+        var divCard = document.createElement("div");
+        var divBody = document.createElement("div");
+        var img = document.createElement("img");
+        var p = document.createElement("p");
+
+        //Adiciona as classes
+        divColunas.classList.add('col-lg-2', 'col-md-4', 'col-6');
+        a.classList.add('d-block', 'mb-2', 'h-100');
+        divCard.classList.add('card', 'text-center', 'width12');
+        divBody.classList.add("card-body");
+        img.classList.add('card-img-top', 'img-thumb');
+        p.classList.add("card-text");
+    
+
+        //Muda as informações
+        img.src = imageGaleria[i];
+        p.innerHTML = citacaoGaleria[i];
+
+        //coloca os elementos na tela
+        var estrutura = document.querySelector("#estruturaGaleria");
+        console.log(estrutura);
+        estrutura.appendChild(divColunas);
+        divColunas.appendChild(a);
+        a.appendChild(divCard);
+        divCard.appendChild(divBody);
+        divBody.appendChild(img);
+        divBody.appendChild(p);
+    }
+}
+
+
--- script.js
+++
@@ -1,4 +0,0 @@
-new fullpage('#fullpage', {
-    licenseKey: 'YOUR KEY HERE',
-    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-  });
